564c41581811f62650c15ead5adbcaeeb8510232
kevinzzzzz <jie736805161@qq.com>
2026-01-31 23:38:36 +0800

    Changes

---
+++ 4-1.关系运算.py
@@ -0,0 +1,12 @@
+a = int(input("输入我们球队的实力:"))
+b = int(input('输入对手1的实力:'))
+c = int(input('输入对手2的实力:'))
+d = int(input('输入对手3的实力:'))
+
+avsb = (a > b) * 3 + (a == b)
+avsc = (a > c) * 3 + (a == c)
+cvsb = (c > b) * 3 + (c == b)
+
+scope = avsb + avsc + cvsb
+
+print("小组赛球队得%d分。"%(scope))
