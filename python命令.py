115de8cc75354c73c04c72c633e56ea22cab6adf
kevinzzzzz <jie736805161@qq.com>
2026-03-31 23:54:08 +0800

    Changes

---
+++ python命令.py
@@ -0,0 +1,11 @@
+"""
+安装指定包  pip3 install 包名
+临时使用镜像地址安装指定包  pip3 install -i 镜像地址 包名
+设置pip的镜像地址：pip3 config set global.index-url 地址
+查看当前环境的pip地址：pip3 config list
+让pip恢复使用官方默认地址： pip3 config unset global.index-url
+列出当前环境中，已经安装的所有第三方包  pip3 list
+从当前环境中卸载指定的第三方包  pip3 uninstall 包
+
+
+"""
